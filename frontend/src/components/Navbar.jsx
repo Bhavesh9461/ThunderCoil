fc0d196d8e4c524a30520eeaadc562ea041a8e76
Bhavesh Kumar <bhaveshaadra26@gmail.com>
2026-03-13 15:45:22 +0530

    feat: improve Navbar and ThemeSelector component styling for better responsiveness

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
   const isChatPage = location.pathname?.startsWith("/chat");
+  const isHomePage = location.pathname?.startsWith("/")
 
   const { logoutMutation } = useLogout();
 
@@ -17,22 +18,22 @@ const Navbar = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-end w-full">
           {/* LOGO - ONLY IN THE CHAT PAGE */}
-          {isChatPage && (
+          {isChatPage || isHomePage && (
             <div className="sm:pl-5">
-              <Link to="/" className="flex items-center gap-2.5">
-                <CloudLightning className="size-5 sm:size-9 text-primary" />
-                <span className="text-xl sm:text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent tracking-wider">
+              <Link to="/" className="flex items-center gap-1.5 sm:gap-2.5">
+                <CloudLightning className="size-4 sm:size-7 md:size-9 text-primary" />
+                <span className="text-md sm:text-xl md:text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent tracking-wider">
                   ThunderCoil
                 </span>
               </Link>
             </div>
           )}
 
-          <div className="flex items-center gap-3 sm:gap-4 ml-auto">
+          <div className="flex items-center gap-1 sm:gap-3 md:gap-4 ml-auto">
             {/* NOTIFICATIONS LINK */}
             <Link to="/notifications">
               <button className="btn btn-ghost btn-circle">
-                <BellIcon className="h-6 w-6 text-base-content opacity-70" />
+                <BellIcon className="h-4 w-4  sm:h-6 sm:w-6 text-base-content opacity-70" />
               </button>
             </Link>
 
@@ -42,7 +43,7 @@ const Navbar = () => {
             {/* USER AVATAR */}
             <Link to="/">
               <div className="avatar">
-                <div className="w-9 rounded-full border border-base-300">
+                <div className="w-7 sm:w-9 rounded-full border border-base-300">
                   <img
                     src={authUser?.profilePic}
                     alt="User Avatar"
@@ -57,7 +58,7 @@ const Navbar = () => {
               className="btn btn-ghost btn-circle"
               onClick={logoutMutation}
             >
-              <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
+              <LogOutIcon className="h-4 w-4  sm:h-6 sm:w-6 text-base-content opacity-70" />
             </button>
           </div>
         </div>
--- frontend/src/components/ThemeSelector.jsx
+++ frontend/src/components/ThemeSelector.jsx
@@ -10,7 +10,7 @@ const ThemeSelector = () => {
     <div className="dropdown dropdown-end">
       {/* DROPDWON TRIGGER */}
       <button tabIndex={0} className="btn btn-ghost btn-circle">
-        <PaletteIcon className="size-5" />
+        <PaletteIcon className="size-4 sm:size-5" />
       </button>
 
       <div tabIndex={0} className="dropdown-content mt-2 p-1 shadow-2xl bg-base-200 backdrop-blur-lg rounded-2xl w-56 border border-base-content/10 max-h-80 overflow-y-auto">
