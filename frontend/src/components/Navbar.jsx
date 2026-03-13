9dc7b30b7b3d0dd21a72c882cc38ba29e39bd088
Bhavesh Kumar <bhaveshaadra26@gmail.com>
2026-03-13 15:32:05 +0530

    feat: enhance Navbar styling for better responsiveness on chat page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,10 +18,10 @@ const Navbar = () => {
         <div className="flex items-center justify-end w-full">
           {/* LOGO - ONLY IN THE CHAT PAGE */}
           {isChatPage && (
-            <div className="pl-5">
+            <div className="sm:pl-5">
               <Link to="/" className="flex items-center gap-2.5">
-                <CloudLightning className="size-9 text-primary" />
-                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent tracking-wider">
+                <CloudLightning className="size-5 sm:size-9 text-primary" />
+                <span className="text-xl sm:text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent tracking-wider">
                   ThunderCoil
                 </span>
               </Link>
