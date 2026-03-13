8bf7c564b071e783dfb4f80f03852b5fddce1920
Bhavesh Kumar <bhaveshaadra26@gmail.com>
2026-03-13 16:10:48 +0530

    fix: improve Navbar layout and responsiveness

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,17 +9,17 @@ const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
   const isChatPage = location.pathname?.startsWith("/chat");
-  const isHomePage = location.pathname?.startsWith("/")
+  const isHomePage = location.pathname === "/"
 
   const { logoutMutation } = useLogout();
 
   return (
     <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-end w-full">
+        <div className="flex items-center justify-between w-full">
           {/* LOGO - ONLY IN THE CHAT PAGE */}
-          {isChatPage || isHomePage && (
-            <div className="sm:pl-5">
+          { (isChatPage || isHomePage ) && (
+            <div className={` ${isHomePage ? "lg:hidden" : "" } sm:pl-5 `}>
               <Link to="/" className="flex items-center gap-1.5 sm:gap-2.5">
                 <CloudLightning className="size-4 sm:size-7 md:size-9 text-primary" />
                 <span className="text-md sm:text-xl md:text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent tracking-wider">
